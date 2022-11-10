ceeffc0955639719d63fea9c196ca64fa59e0a79
cornuel <ultimn8@gmail.com>
2022-11-10 17:37:00 +0100

    first project with channels

---
+++ app/celery.py
@@ -0,0 +1,17 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+
+app = Celery('app')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = {
+    'get_coins_data_30s': {
+        'task': 'coins.tasks.get_coins_data',
+        'schedule': 10.0
+    }
+}
+
+app.autodiscover_tasks()
